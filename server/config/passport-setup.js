dcb238a5ab5edf5aaee29736cb30d71561eb8a71
Charles <okoyecharles509@gmail.com>
2023-08-31 10:40:25 +0100

    Store logged in users in db

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,15 +1,31 @@
 import passport from "passport";
 import GoogleStrategy from "passport-google-oauth20";
+import User from "../models/user-model.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://localhost:${process.env.PORT}/auth/google/redirect`
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log(profile);
-  })
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `http://localhost:${process.env.PORT}/auth/google/redirect`,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      try {
+        const userExists = await User.findOne({ googleId: profile.id })
+        if (userExists) {
+          console.log("user already exists");
+        } else {
+          const newUser = await new User({
+            username: profile.displayName,
+            googleId: profile.id,
+          }).save();
+          console.log("new user created", newUser);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  )
 );

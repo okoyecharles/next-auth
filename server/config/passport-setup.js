5b0ab6d6302820041fb94a0b09206622561b153e
Charles <okoyecharles509@gmail.com>
2023-08-31 10:49:02 +0100

    Serialize and deserialize user object

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -4,6 +4,15 @@ import User from "../models/user-model.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,15 +22,17 @@ passport.use(
     },
     async function (accessToken, refreshToken, profile, done) {
       try {
-        const userExists = await User.findOne({ googleId: profile.id })
+        const userExists = await User.findOne({ googleId: profile.id });
         if (userExists) {
           console.log("user already exists");
+          done(null, userExists);
         } else {
           const newUser = await new User({
             username: profile.displayName,
             googleId: profile.id,
           }).save();
           console.log("new user created", newUser);
+          done(null, newUser);
         }
       } catch (error) {
         console.error(error);
